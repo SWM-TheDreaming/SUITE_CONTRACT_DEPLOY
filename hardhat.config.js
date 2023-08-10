1b68ef64b320e81bd1aad4a61eda31564ae3d179
반영환 <lopahn2@gmail.com>
2023-08-11 01:09:23 +0900

    feat: hardhat.config.js 작성

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,42 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    polygon_testnet: {
+      // Polygon (Matic) 테스트넷 설정
+      url:
+        process.env.POLYGON_TEST_NET_RPC_PROVIDER_URL + process.env.RPC_API_KEY, // Polygon Mumbai Testnet RPC URL
+      accounts: [process.env.WALLET_PRIVATE_KEY], // 지갑 pk 주소
+    },
+    polygon_mainnet: {
+      // Polygon (Matic) 메인넷 설정
+      url:
+        process.env.POLYGON_MAIN_NET_RPC_PROVIDER_URL + process.env.RPC_API_KEY, // Polygon Mainnet RPC URL
+      accounts: [process.env.WALLET_PRIVATE_KEY], // 지갑 pk 주소
+    },
+  },
+  solidity: {
+    version: "0.8.18", // solidity compiler version
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
 };
